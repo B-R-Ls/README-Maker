a24055defaffc18973f6ad85da35a97662988800
Brendan <brendan.r.le@gmail.com>
2023-10-18 10:09:01 -0500

    added renderLicenseBadge code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of the Project?',
+'In one sentence, what does it do?', 
+'Give a in depth description of the project and well as your thoughts, struggles, and successes.',
+'Please provide an Installation link:',
+'Please provide a deployed link:',
+'How do you use your project, and what does it accomplish?',
+'Please put all resource links and contributers in, once finished enter blank.',
+'What License did you use?'
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let lowerLic = license.toLowerCase();
+
+  switch (lowerLic) {
+    case 'apache':
+      console.log('added Apache license.');
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case 'boost':
+      console.log('added Boost license.');
+      return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+      break;
+    case 'eclipse':
+      console.log('added Eclipse license.');
+      return`[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      break;
+    case 'ibm':
+      console.log('added IBM license.');
+      return`[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+      break;
+    case 'isc':
+      console.log('added ISC license.');
+      return`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+      break;
+    case 'mit':
+      console.log('added MIT license.');
+      return`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    default:
+      console.log('That license is not in our database, please try again.');
+      return `tryAgain`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -18,3 +54,5 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+renderLicenseBadge('aasd');
